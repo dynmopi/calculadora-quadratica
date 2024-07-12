f5aaf818d5d5feb010c7e06751af357736cd8146
danrley <maranhaodanrley@gmail.com>
2024-07-11 21:45:50 -0300

    projeto-final1

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -1,17 +1,39 @@
-package org.example;
+package com.calculator;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
+// import static java.lang.Math.sqrt;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public static void main(String[] args) {
+            String a_txt = JOptionPane.showInputDialog("Insira o valor de A: ");
+            String b_txt = JOptionPane.showInputDialog("Insira o valor de B: ");
+            String c_txt = JOptionPane.showInputDialog("Insira o valor de C: ");
+            double a = Integer.parseInt(a_txt);
+            double b = Integer.parseInt(b_txt);
+            double c = Integer.parseInt(c_txt);
+
+            double delta = b*b - 4*a*c;
+            // System.out.println(delta);
+           if (delta > 0){
+               double raiz1 = (-b + Math.sqrt(delta)) / (2*a);
+               double raiz2 = (-b - Math.sqrt(delta)) / (2*a);
+               JOptionPane.showMessageDialog(null, "Temos duas raízes reais e diferentes.");
+               JOptionPane.showMessageDialog(null, "Considerando x' e x'' temos: ");
+
+               JOptionPane.showMessageDialog(null, "x' = "
+                       + String.format("%.3f", raiz1)
+                       + " x'' = "+ String.format("%.3f", raiz2));
+
+            } else if (delta == 0){
+                double raiz = (-b)/(2*a);
+               JOptionPane.showMessageDialog(null,
+                       "Temos uma raíz");
+               JOptionPane.showMessageDialog(null,
+                       "x = " + String.format("%.1f", raiz));
+            } else {
+               JOptionPane.showMessageDialog(null,
+                       "Não temos raízes reais");
+            }
         }
     }
-}
